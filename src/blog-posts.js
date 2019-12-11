34c6b8df84db7822785fc9198b07dbe60a6ffc20
HacerBusraKILIC <hacerbusrakilic@hotmail.com>
2019-12-11 22:54:09 +0300

    Added en-güncel-yazi

--- src/blog-posts.js
+++ src/blog-posts.js
@@ -1,6 +1,12 @@
 export const getPosts = () => {
     return [
         {
+            title: "En güncel yazı",
+            slug: "en-güncel-yazi",
+            detail: require(`./posts/en-güncel-yazi.md`).default,
+            date: "13 Aralık 2019"
+        },
+        {
             title: "Bir React Uygulamasında En Ala SQL Veritabanını Kullanmak",
             slug: "Bir-React-Uygulamasinda-En-Ala-SQL-Veritabanini-Kullanmak",
             detail: require(`./posts/Bir-React-Uygulamasinda-En-Ala-SQL-Veritabanini-Kullanmak.md`).default,
